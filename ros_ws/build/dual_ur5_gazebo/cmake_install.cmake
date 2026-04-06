c9783ef5caa13545e5b55dacd5ff4298bb63d186
Guillermo Mendoza <guillermodmen@gmail.com>
2026-04-05 22:29:41 -0400

    inital sim design

---
+++ ros_ws/build/dual_ur5_gazebo/cmake_install.cmake
@@ -0,0 +1,67 @@
+# Install script for directory: /workspace/ros_ws/src/dual_ur5_gazebo
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/workspace/ros_ws/install")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/workspace/ros_ws/build/dual_ur5_gazebo/catkin_generated/installspace/dual_ur5_gazebo.pc")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dual_ur5_gazebo/cmake" TYPE FILE FILES
+    "/workspace/ros_ws/build/dual_ur5_gazebo/catkin_generated/installspace/dual_ur5_gazeboConfig.cmake"
+    "/workspace/ros_ws/build/dual_ur5_gazebo/catkin_generated/installspace/dual_ur5_gazeboConfig-version.cmake"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dual_ur5_gazebo" TYPE FILE FILES "/workspace/ros_ws/src/dual_ur5_gazebo/package.xml")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/dual_ur5_gazebo" TYPE PROGRAM FILES "/workspace/ros_ws/build/dual_ur5_gazebo/catkin_generated/installspace/spawn_scene.py")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dual_ur5_gazebo" TYPE DIRECTORY FILES
+    "/workspace/ros_ws/src/dual_ur5_gazebo/config"
+    "/workspace/ros_ws/src/dual_ur5_gazebo/launch"
+    "/workspace/ros_ws/src/dual_ur5_gazebo/models"
+    "/workspace/ros_ws/src/dual_ur5_gazebo/worlds"
+    )
+endif()
+
